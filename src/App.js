6126468ed19154febd6ea2b8a8001123fe4d7a6f
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2024-02-03 20:23:57 -0500

    countries information

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import { useEffect, useState } from 'react';
 import Country from './components/Country';
 
 function App() {
-  const [country,setCountry] = useState([]);
+  const [countries,setCountry] = useState([]);
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(data =>{setCountry(data)
-      // const name = data.map(country =>country.name.common);
-      // console.log(name);
+      const name = data.map(country =>country.name.common);
+      console.log(name);
     });
    
    
   },[])
   return (
     <div className="App">
-      <Country country={country} ></Country>
+     {countries.map(country => <Country country={country} ></Country>) }
         
     </div>
   );
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,19 +1,24 @@
 import React from 'react';
-
+import './Country.css';
 const Country = (props) => {
     const countries = props.country;
   
-  
+ 
+    console.log(countries);
 
     return (
         <div>
-            <h1>Country Loaded:{countries.length}</h1>
-
-            <ul>
-               {
-                countries.map(country => <li>{country.name.common}</li>)
-               } 
-            </ul>
+           <div className='countryInfo'>
+               <img src={countries.flags.png} alt="" />
+               <div className="description">
+               <h2>{countries.name.common}</h2>
+                <h4>Population: {countries.population}</h4>
+                <h4>Capital: {countries.capital}</h4>
+                 <h4>Region: {countries.region}</h4>
+                 <img src={countries.maps.googleMaps} alt="" />
+               </div>   
+           </div>
+            
         </div>
     );
 };
