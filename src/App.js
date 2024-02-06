0a6ae11abfa784994df2a3f9919947c58ce24db2
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2024-02-06 07:11:49 -0500

    cart commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import Country from './components/Country';
 
 function App() {
   const [countries,setCountry] = useState([]);
+  const [cart, setCart] = useState([]);
+  
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(data =>{setCountry(data)
       const name = data.map(country =>country.name.common);
-      console.log(name);
+    
     });
    
    
   },[])
+  const handleClick = (country) =>{
+    const newCart = [...cart,country];
+    setCart(newCart);
+  }
   return (
     <div className="App">
-     {countries.map(country => <Country country={country} ></Country>) }
-        
+      <div className="navBar">
+        <h5>Your countries:{cart.length}</h5>
+        </div> 
+     {countries.map(country => <Country country={country} clickHandle={handleClick} ></Country>) }
+       
     </div>
   );
 }
--- src/components/Country.js
+++ src/components/Country.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Country.css';
 const Country = (props) => {
     const countries = props.country;
-  
+    const clickHandle = props.clickHandle;
  
-    console.log(countries);
+    
 
     return (
         <div>
@@ -16,6 +16,7 @@ const Country = (props) => {
                 <h4>Capital: {countries.capital}</h4>
                  <h4>Region: {countries.region}</h4>
                  <img src={countries.maps.googleMaps} alt="" />
+                 <button onClick={()=>clickHandle(countries)}>Add the country</button>
                </div>   
            </div>
             
